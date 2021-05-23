9826c7fd44da1e68f619f0542948276a3aa612c3
JoeAshraf <youssefashraf397@yahoo.com>
2021-05-24 01:47:13 +0200

    add goodes to homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Good } from 'src/app/interfaces/goodInterface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  goods: Good[] = [
+   { name: 'apple' , price: 5 , photo: 'assets/apple.jpeg'} ,
+   { name: 'banana' , price: 10 , photo: 'assets/banana.jpeg'} ,
+   { name: 'strwberry' , price: 20 , photo: 'assets/strwberry.jpeg'} ,
+   { name: 'mango' , price: 60 , photo: 'assets/mango.jpeg'} ,
+   { name: 'kiwi' , price: 35 , photo: 'assets/kiwi.jpeg'} ,
+  ] ;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addToCart(index){
+    console.log("added" , this.goods[index]) ;
+
   }
 
 }
