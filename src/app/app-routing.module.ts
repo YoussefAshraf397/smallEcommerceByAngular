ea846bf8cb772ac64819e1365cd0a900810d6aea
JoeAshraf <youssefashraf397@yahoo.com>
2021-05-23 21:36:15 +0200

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { GoodsComponent } from './components/goods/goods.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 const routes: Routes = [
 {path: '' , component: HomeComponent},
 {path: 'login' , component: LoginComponent} , 
+{path: 'signup' , component: SignupComponent} , 
 {path: 'cart' , component: CartComponent} , 
 {path: 'admin' , component: GoodsComponent} , 
 {path: '**' , component: NotFoundComponent}
