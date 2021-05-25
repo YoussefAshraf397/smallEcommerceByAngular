a82d7a995412a7227c559d7063a4c19845f29de9
JoeAshraf <youssefashraf397@yahoo.com>
2021-05-25 02:09:58 +0200

    intialize form of signup and make validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -26,7 +26,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,4 +12,9 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  signUp(form)
+  {
+    console.log(form);
+  }
+
 }
